bf8f1000384698905fed6027f3658a472714f315
makireland <makireland@gmail.com>
2021-03-25 15:52:05 +0000

    SellerService and basic FindAll

---
+++ SalesWebMvcApp/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvcApp.Data;
+using SalesWebMvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcApp.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcAppContext _context;
+
+        public SellerService(SalesWebMvcAppContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
