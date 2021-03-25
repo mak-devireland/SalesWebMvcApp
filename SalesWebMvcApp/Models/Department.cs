0dcfd1655c82441815003d72e2b9de0bc134c581
makireland <makireland@gmail.com>
2021-03-25 11:01:40 +0000

    Other Entities and Second Migration

--- SalesWebMvcApp/Models/Department.cs
+++ SalesWebMvcApp/Models/Department.cs
@@ -9,6 +9,7 @@ namespace SalesWebMvcApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
@@ -18,6 +19,16 @@ namespace SalesWebMvcApp.Models
         {
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, final));
         }
     }
 }
---
+++ SalesWebMvcApp/Models/Seller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcApp.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateBirth { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime dateBirth, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            DateBirth = dateBirth;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            SalesRecords.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            SalesRecords.Remove(sales);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
