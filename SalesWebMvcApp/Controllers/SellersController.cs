2c52032a88921edd27417ee3fac022d07dfbcd91
makireland <makireland@gmail.com>
2021-04-02 18:43:28 +0100

    Update seller and custom service exception

--- SalesWebMvcApp/Controllers/SellersController.cs
+++ SalesWebMvcApp/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvcApp.Models;
 using SalesWebMvcApp.Models.ViewModels;
 using SalesWebMvcApp.Services;
+using SalesWebMvcApp.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -81,6 +82,53 @@ namespace SalesWebMvcApp.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            var department = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Seller = obj, Departments = department };
+           
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+           
         }
     }
 }
