bb2ab332b91624253a5623ddb1903f017e111d7f
makireland <makireland@gmail.com>
2021-04-04 00:14:09 +0100

    Validation

--- SalesWebMvcApp/Models/Seller.cs
+++ SalesWebMvcApp/Models/Seller.cs
@@ -2,27 +2,35 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvcApp.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} field can't be empty")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} to {1} characters")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} field can't be empty")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} field can't be empty")]
         [Display(Name = "Date of Birth")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DateBirth { get; set; }
 
+        [Required(ErrorMessage = "{0} field can't be empty")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+
+        [Required(ErrorMessage = "{0} field can't be empty")]
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
